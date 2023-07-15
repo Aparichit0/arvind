edfdaa6c3d04159e7568382daf1aa5b49b1df1fd
Dharmveer singh <93782780+Aparichit0@users.noreply.github.com>
2023-07-15 16:07:37 +0530

    updated script & style
    Moved cake.gif to a better location,
    New background for music playback

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
-// reused from kd
 function musicPlayback() {
     let song = document.querySelector("#song");
     let quote = document.querySelector("blockquote");
     let giftbox = document.querySelector(".giftbox");
+    let background = document.querySelector("main");
+    //replace giftbox icon for first time
+    if (giftbox.style.backgroundImage != "url(./resources/cake.gif)")
+    {giftbox.style.backgroundImage = "url(./resources/cake.gif)"}
+    //music playback handling
     if (song.paused == true) {
         //  play music, hide giftbox and display quote 
         song.play();
-        quote.removeAttribute("hidden")
-        giftbox.setAttribute("hidden", true)
+        quote.removeAttribute("hidden");
+        giftbox.setAttribute("hidden", true);
+        //change background with playback
+        background.style.backgroundImage = "url(./resources/drinks.jpeg)";
     } else {
         // if playing the pause and hide quote and make giftbox visible
         song.pause();
-        quote.setAttribute("hidden", true)
-        giftbox.removeAttribute("hidden")
+        quote.setAttribute("hidden", true);
+        giftbox.removeAttribute("hidden");
+        background.style.backgroundImage = "url(./resources/ivana-cajina-HDd-NQ_AMNQ-unsplash.jpg)";
     }
 }
